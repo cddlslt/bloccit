93dfa6dbd9d915004d2062456b6036ad425782a7
cddlslt <oatesmi.74@gmail.com>
2018-01-16 19:09:55 -0600

    SponsoredPosts controller built

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sponsored_post/show'
+
+  get 'sponsored_post/new'
+
+  get 'sponsored_post/edit'
+
   resources :topics do
     resources :posts, except: [:index]
   end
