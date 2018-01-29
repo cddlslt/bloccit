79c6976840be0bab0ca96de8709bc4e6ddd19b03
cddlslt <oatesmi.74@gmail.com>
2018-01-29 09:43:05 -0600

    rails: authentication cp complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
-  before_save :format_name
+  # before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
@@ -11,14 +11,14 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
   has_secure_password
 
-  def format_name
-    if name.present?
-      name_array = []
-      name.split.each do |x|
-        name_array << x.capitalize
-      end
-
-      self.name = name_array.join(" ")
-    end
-  end
+  # def format_name
+  #   if name.present?
+  #     name_array = []
+  #     name.split.each do |x|
+  #       name_array << x.capitalize
+  #     end
+  #
+  #     self.name = name_array.join(" ")
+  #   end
+  # end
 end
